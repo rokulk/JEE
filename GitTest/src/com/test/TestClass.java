46cd33db79b220884ee0415cbbeeabfac1850e0e
rokulk <warunaonline@gmail.com>
2017-03-04 21:35:39 +0530

    new commit

--- GitTest/src/com/test/TestClass.java
+++ GitTest/src/com/test/TestClass.java
@@ -5,7 +5,7 @@ public class TestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello GIT version 1");
-		System.out.println("Hello GIT version 3");
+		System.out.println("Hello GIT version 4");
 	}
 
 }
