d4012aba9e1bca7e79e1be9c82bd7853491bb000
rokulk <warunaonline@gmail.com>
2017-06-12 21:02:57 +0530

    Update RoshangaSpringBootApplication.java

--- RoshangaSpringBoot/src/main/java/com/rokulk/main/RoshangaSpringBootApplication.java
+++ RoshangaSpringBoot/src/main/java/com/rokulk/main/RoshangaSpringBootApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RoshangaSpringBootApplication {
 
-	public static void main(String[] args123) {
+	public static void main(String[] args1223) {
 		SpringApplication.run(RoshangaSpringBootApplication.class, args123);
 	}
 }
